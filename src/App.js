f45d9dd1459cad01c3c7edbb76112b478ee76493
Chetan <chetan87706@gmail.com>
2025-02-20 20:22:26 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+  let toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#072e67";
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = "white";
+    }
+  }
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        {/* <Navbar/> */}
+        <Routes>
+            <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm heading="Enter text to analyze!!" mode={mode} />}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
